810e7e1cca32b78faaf8bd0bfbd094034a25dde4
Ben Hosmer <ben.hosmer@gmail.com>
2017-05-16 10:30:53 -0400

    Fix #1 validate yaml before continuing

--- gh2gl/gh2gl.py
+++ gh2gl/gh2gl.py
@@ -6,6 +6,7 @@ import yaml
 import sys
 import argparse
 import requests
+from urlparse import urlparse
 
 def parse_args(args):
     """Get the command-line arguments to pass in the config file
@@ -14,6 +15,34 @@ def parse_args(args):
     parser.add_argument('config', help='yaml file containing repo urls')
     parser.add_argument('--apitoken', help='gitlab api token')
     return parser.parse_args()
+
+def validate_datafile():
+    parser = parse_args(sys.argv[1:])
+
+    # Ensure an actual valid file was provided
+    try:
+        with open(parser.config) as datafile:
+            repodata = yaml.load(datafile)
+    except IOError:
+        sys.exit('Cannot read file %s' % (parser.config))
+
+    # Ensure proper keys and values in the file
+    try:
+        gitlaburls = repodata.keys()
+        for gitlaburl in gitlaburls:
+            if ('http' or 'https') not in urlparse(gitlaburl).scheme:
+                raise KeyError('No valid gitlab url provided %s' % (gitlaburl))
+            for item in repodata[gitlaburl]:
+                if ('github' or 'gitlabgid') not in repodata[gitlaburl][item].keys():
+                    raise KeyError('No gitlabgid or github for %s %s' % (item, repodata[gitlaburl][item].keys()))
+                if ('http' or 'https') not in urlparse(repodata[gitlaburl][item]['github']).scheme:
+                    raise ValueError('No valid github url provided %s %s' % (item, repodata[gitlaburl][item]['github']))
+                if not isinstance(repodata[gitlaburl][item]['gitlabgid'], int):
+                    raise ValueError('No gitlabgid %s %s' % (item, repodata[gitlaburl][item]['gitlabgid']))
+    except Exception as e:
+        sys.exit('Error in config file: %s' % (e))
+
+    return repodata
 
 
 def createrepos():
@@ -25,15 +54,10 @@ def createrepos():
             gitlabtoken = os.environ['GITLAB_API_PRIVATE_TOKEN']
         else:
             gitlabtoken = parser.apitoken
-    except KeyError:
-        sys.exit('No API private token provided')
-
-    try:
-        with open(parser.config) as datafile:
-            repodata = yaml.load(datafile)
-    except IOError:
-        sys.exit('Cannot read file')
-
+    #except KeyError:
+    except Exception as e:
+        sys.exit('No API private token provided %s' % (e))
+    repodata = validate_datafile()
     headers = {'PRIVATE-TOKEN': gitlabtoken}
     gitlaburls = repodata.keys()
     for gitlaburl in gitlaburls:
@@ -46,6 +70,5 @@ def createrepos():
             print r.text
 
 if __name__ == "__main__":
-    parse_args(sys.argv[1:])
     createrepos()
 
